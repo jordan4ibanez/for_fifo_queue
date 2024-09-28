2102e4f355240778594e91870515f2ba2fb65cd6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-28 06:20:23 -0400

    internal_new_fifo_queue

--- src/fifo_queue_bindings.f90
+++ src/fifo_queue_bindings.f90
@@ -8,7 +8,13 @@ module fifo_queue
   interface
 
 
-      function internal_new_fifo_queue(fortran_data_width)
+    function internal_new_fifo_queue(fortran_data_width) result(fifo_pointer) bind(c, name = "new_fifo_queue")
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      integer(c_size_t), intent(in), value :: fortran_data_width
+      type(c_ptr) :: fifo_pointer
+    end function internal_new_fifo_queue
 
 
   end interface
