4f257ff795bc91f64fd1fa4d7a320a7529089a2b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-28 05:14:31 -0400

    layout some base data

--- src/fifo_queue.c
+++ src/fifo_queue.c
@@ -3,8 +3,28 @@
 #include <stdbool.h>
 
 typedef struct header header;
+typedef struct fifo_queue fifo_queue;
 
+/**
+ * Simple header to allow quickly jumping over the Fortran data.
+ */
 struct header
 {
   char *next;
 };
+
+/**
+ * The base struct that controls the underlying pointer data.
+ */
+struct fifo_queue
+{
+  char *head;
+  char *tail;
+  size_t element_size;
+  size_t fortran_data_size;
+  size_t count;
+};
+
+char *new_fifo_queue(size_t fortran_data_width)
+{
+}
